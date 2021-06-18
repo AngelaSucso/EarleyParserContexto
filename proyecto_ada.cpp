3e3299df6bd0d32e1420a5b28dc302a676d2c890
dpuma <dpumar3@gmail.com>
2021-06-17 22:47:12 -0500

    estados iniciales chart

--- proyecto_ada.cpp
+++ proyecto_ada.cpp
@@ -45,7 +45,7 @@ struct Produccion
 };
 
 void Produccion::imprimir(){
-    cout<<"[f]"<<first<<"   "<<"[s]"<<second<<endl;
+    cout<<"[f]"<<first<<" "<<"[s]"<<second;
 }
 
 class EarleyState
@@ -58,35 +58,42 @@ class EarleyState
     EarleyState* referencia_ptr;
 public:
     EarleyState(Produccion, int, int, int, int);
+    friend class EarleyParser;
 };
 
 EarleyState::EarleyState(Produccion prod, int pp, int pc, int ec, int r)
 {
     produccion_actual.first = prod.first;
     produccion_actual.second = prod.second;
-    pos_punto = pp;
-    pos_chart = pc;
-    estado_chart = ec;
-    referencia_int = r;
+    pos_punto = pp;     // pos del punto en la expresion
+    pos_chart = pc;     // linea del chart actual
+    estado_chart = ec;  // 0(inicio,predecir), 1(escanear,completar), 2(escanear,predecir), 3(... )
+    referencia_int = r; // de donde viene la produccion actual
 }
 
 class EarleyParser
 {
-    vector<Produccion> P;
-    vector<string> SNT; // simbolos no terminales
-    vector<string> ST;  // simbolos terminales
-    string Inicial;
+    vector<Produccion> P;   // vector de producciones
+    vector<string> SNT;     // simbolos no terminales
+    vector<string> ST;      // simbolos terminales
+    string inicial;         // estado inicial
     string expresion;
 
     vector<EarleyState*> chart; // Chart es un arreglo de EarlyState
     // (...)
 public:
     EarleyParser(vector <string>);
+    void recibirEntrada(vector<string>);
+    void imprimirChart();
+    void predecir();
+    void completar();
+    void escanear();
 };
 
-EarleyParser::EarleyParser(vector <string> entrada){
+void EarleyParser::recibirEntrada(vector<string> entrada)
+{
     expresion = entrada[0];
-    Inicial = entrada[1];
+    inicial = entrada[1];
     string lexema;
     for(int i=0; i<entrada[2].size(); i++){
         if(entrada[2][i]!=','){
@@ -133,7 +140,7 @@ EarleyParser::EarleyParser(vector <string> entrada){
 
     cout<<"DATOS RECIBIDOS"<<endl;
     cout<<"Expresion      : "<<expresion<<endl;
-    cout<<"Estado Inicial : "<<Inicial<<endl;
+    cout<<"Estado inicial : "<<inicial<<endl;
     cout<<"No Terminales  : ";
     imprimirVector(SNT);
     cout<<endl;
@@ -143,10 +150,74 @@ EarleyParser::EarleyParser(vector <string> entrada){
     cout<<"PRODUCCIONES"<<endl;
     for(int i=0; i<P.size(); i++){
         P[i].imprimir();
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void EarleyParser::imprimirChart()
+{
+    cout << "============== CHART ==============" << endl;
+    cout << "pos_chart, produc_actual, pos_punto, estado, referencia\n\n";
+    for(int i = 0; i < chart.size(); i++)
+    {
+        cout << chart[i]->pos_chart << ", ";
+        chart[i]->produccion_actual.imprimir();
+        cout << ", " << chart[i]->pos_punto << ", " << chart[i]->estado_chart << ", " << chart[i]->referencia_int << endl;
     }
 }
 
-//int fun()
+void EarleyParser::predecir()
+{
+
+}
+
+void EarleyParser::completar()
+{
+
+}
+
+void EarleyParser::escanear()
+{
+
+}
+
+/*
+constructor:
+{
+    recibirEntrada();
+    insertar primer estado en chart
+    while()
+    {
+        predecir();
+        escanear();
+        completar();
+        escanear();
+    }
+}
+*/
+
+EarleyParser::EarleyParser(vector <string> entrada)
+{
+    recibirEntrada(entrada);
+
+    int pos_ch = 0;     // inicio pos_chart
+    int estado_ch = 0;  // inicio estado_chart
+    for(int i = 0; i < P.size(); i++)
+    {
+        if (P[i].first == inicial)
+        {
+            EarleyState* oEarley = new EarleyState(P[i], 0, pos_ch, estado_ch, 0);
+            chart.push_back(oEarley);
+            pos_ch++;
+        }
+    }
+
+    // test
+    imprimirChart();
+
+}
+
 
 // hacer el input - identificar la gramatica y la expresion a evaluar
 int main()
